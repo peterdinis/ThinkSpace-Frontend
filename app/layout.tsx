8c5958dc70450504a93f0c6293f55d70db956c3c
Peter Dinis <peterdinis0@pobox.sk>
2024-10-26 13:34:11 +0200

    infra: Biome config add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,22 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Think Space",
-  description: "A daily use app for everything",
+	title: "Think Space",
+	description: "A daily use app for everything",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`antialiased`}>{children}</body>
+		</html>
+	);
 }
